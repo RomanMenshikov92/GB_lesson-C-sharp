0fc108f3335c9505cc24c9e518cd55760659c676
Roman Menshikov <R-Tupac@yandex.ru>
2022-07-20 23:43:48 +0300

    add Tack_0

---
+++ Tack_0/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Tack_0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello 12 World!");
+        }
+    }
+}
