8bc300d4448b69c65d1df4f8f74267a50d166155
Roman Menshikov <R-Tupac@yandex.ru>
2022-07-21 00:16:07 +0300

    add example1_Tack0

--- Tack_0/Program.cs
+++ Tack_0/Program.cs
@@ -5,8 +5,21 @@ namespace Tack_0
     class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello 12 World!");
+        {   
+//                 string str = "Привет!";
+// Console.while ("Введите целое число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+int num = 4;
+int res = num * num;
+Console.WriteLine(res);
+// Console.WriteLine($"Квадрат числа {num} = {res}");
         }
     }
 }
+/*Напишите программу, которая на вход
+принимает число и выдаёт его квадрат (число
+умноженное на само себя)
+4 -> 16
+-3 -> 9
+-7 -> 49
+*/
