80558eec636197a2bdb76b6ddbc39362236dd494
Roman Menshikov <R-Tupac@yandex.ru>
2022-07-25 00:16:39 +0300

    add Tack_2_Homework-1.1

---
+++ Tack_2_Homework-1.1/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+//456-> 5
+//782-> 8
+//918-> 1
+int threeDigitNumber;
+string stringIndex;
+
+Console.WriteLine("Введи трёхзначное число: ");
+threeDigitNumber = Convert.ToInt32(Console.ReadLine());
+stringIndex = Convert.ToString(threeDigitNumber);
+    if (threeDigitNumber < 999) {
+        Console.WriteLine("Вторая цифра этого числа -> " + stringIndex[1]);
+    }
+    else {
+        Console.WriteLine("Это не трехзначное число");
+    }
+Console.ReadLine();
+
