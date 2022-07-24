2bd335b22d1dee046b01ad8b5432a66c1a9579b1
Roman Menshikov <R-Tupac@yandex.ru>
2022-07-25 00:24:49 +0300

    add Tack_2_Homework-1.2

---
+++ Tack_2_homework-1.2/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+//645-> 5
+//78->третьей цифры нет
+//32679 -> 6
+int num;
+string stringIndex;
+
+Console.Write("Введите любое число: ");
+num = Convert.ToInt32(Console.ReadLine());
+stringIndex = Convert.ToString(num);
+    if (stringIndex.Length > 2) {
+        Console.WriteLine("третья цифра -> " + stringIndex[2]); 
+    }
+    else {
+        Console.WriteLine("-> третьей цифры нет");
+    }
+Console.ReadLine();
