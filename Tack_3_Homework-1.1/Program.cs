f2b173fabf7aa42ba7e6ff43f295506d3a4736cb
Roman Menshikov <R-Tupac@yandex.ru>
2022-08-14 18:58:00 +0300

    added next homework-4

--- Tack_3_Homework-1.1/Program.cs
+++ Tack_3_Homework-1.1/Program.cs
@@ -3,21 +3,21 @@
 // 14212 -> нет
 // 12821 -> да
 // 23432 -> да
-string number;
-Console.WriteLine("Введите пятизначное число: ");
-number = Console.ReadLine();
+// string number;
+// Console.WriteLine("Введите пятизначное число: ");
+// number = Console.ReadLine();
 
-void CheckingNumber(string number)
-{
-    if (number[0] == number[4] || number[1] == number[3]) {
-        Console.WriteLine($"Ваше число: {number} - Это палиндром.");
-    }
-    else Console.WriteLine($"Ваше число: {number} - Это не палиндром.");
-}
+// void CheckingNumber(string number)
+// {
+//     if (number[0] == number[4] || number[1] == number[3]) {
+//         Console.WriteLine($"Ваше число: {number} - Это палиндром.");
+//     }
+//     else Console.WriteLine($"Ваше число: {number} - Это не палиндром.");
+// }
 
-if (number.Length == 5) {
-    CheckingNumber(number);
-}
-else Console.WriteLine($"Пожалуйста, введите пятизначное число");
+// if (number.Length == 5) {
+//     CheckingNumber(number);
+// }
+// else Console.WriteLine($"Пожалуйста, введите пятизначное число");
 
-Console.ReadLine();
+// Console.ReadLine();
