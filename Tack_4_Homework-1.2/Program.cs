494ee536246a9a8d221e0f4a8d9e999f2b8f4f71
Roman Menshikov <R-Tupac@yandex.ru>
2022-08-14 19:01:04 +0300

    added next homework-4

---
+++ Tack_4_Homework-1.2/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.Write("Введите число N: ");
+int numN = Convert.ToInt32(Console.ReadLine());
+
+int SumNumN(int numN)
+{
+    int result = 0;
+
+    for (int i = 0; i < numN; i++)
+    {
+        result = result + numN % 10;
+        numN = numN / 10;
+    }
+    return result;
+}
+
+int sumNumN = SumNumN(numN);
+Console.WriteLine("Сумма цифр в числе: " + sumNumN);
---
+++ Tack_4_Homework-1.3/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов, заполненный
+// псевдослучайными числами и выводит их на экран.
+// 1, 2, 5, 7, 19, 6, 1, 33 -> [1, 2, 5, 7, 19, 6, 1, 33]
+
+int[] numbers = GetRandomArray(8);
+
+int[] GetRandomArray(int arrayLength)
+{
+    int[] arr = new int[arrayLength];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(0, 99);
+    }
+    return arr;
+}
+
+void WriteRandomArrayToConsole(int[] numbers)
+{   
+    Console.Clear();
+    Console.Write("[");
+    for (int i = 0; i < numbers.Length; i++)
+    {   
+        Console.Write(numbers[i]);
+        if (i != numbers.Length - 1)
+            Console.Write(",");
+    }
+    Console.WriteLine("]");
+}
+
+WriteRandomArrayToConsole(numbers);
