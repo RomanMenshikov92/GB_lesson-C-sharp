29427d7862770c57e2f8873adb9c5e087fbe7728
Roman Menshikov <R-Tupac@yandex.ru>
2022-08-16 22:07:04 +0300

    added next homework-6

---
+++ Tack_6_Homework-1.1/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// -1, -7, 567, 89, 223-> 3
+
+Console.Clear();
+Console.Write($"Каков ваш размер массива?: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[size];
+
+void EnteringNumbers(int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($"Введите {i + 1} элемент c клавиатуры: ");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+}
+EnteringNumbers(size);
+
+int Check(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0) count += 1;
+    }
+    return count;
+}
+int checkout = Check(array);
+Console.WriteLine($"В данном массиве мы обнаружили, что числа больше 0 равно : {checkout} ");
---
+++ Tack_6_Homework-1.2/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9-> (-0, 5; -0,5)
+
+Console.Clear();
+double[ , ] arr = new double[2 , 2];
+double[] point = new double[2];
+
+void EnteringNumbersInAnArray()
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        Console.Write($"Введите числа {i + 1}-го уравнения:\n");
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            if (j == 0) Console.Write($"Введите - k{j + 1}: ");
+            else Console.Write($"Введите - b{i + 1}: ");
+            arr[i, j] = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}
+EnteringNumbersInAnArray();
+
+double[] Equation(double[ , ] arr)
+{
+    point[0] = (arr[1, 1] - arr[0, 1]) / (arr[0, 0] - arr[1, 0]);
+    point[1] = point[0] * arr[0, 0] + arr[0, 1];
+    return point;
+}
+Equation(arr);
+
+Console.WriteLine($"Мы получили точки пересечения прямых: ({point[0]}; {point[1]})");
+
